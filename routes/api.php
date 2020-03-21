683c4df50e5a9555febb398cd21c9fccec52da07
root <root@localhost.localdomain>
2020-03-21 23:00:37 +0630

    re

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-
+Route::post('/auth/register','Api\AuthController@register');
 Route::post('/auth/token', 'Api\AuthController@getAccessToken');
 Route::post('/auth/reset-password', 'Api\AuthController@passwordResetRequest');
 Route::post('/auth/change-password', 'Api\AuthController@changePassword');
